6e97ba000088509f45dbaf88c391bd11a619b5a4
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2017-08-20 16:30:22 +0600

    Bubble sort implementation

--- src/test/java/sorting/bubblesort/BubbleSortTest.java
+++ src/test/java/sorting/bubblesort/BubbleSortTest.java
@@ -1,8 +1,31 @@
 package sorting.bubblesort;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author asif.hossain
  * @since 7/27/17.
  */
 public class BubbleSortTest {
+    @Test
+    public void emptyArrayReturnsEmpty() {
+        int[] a = new int[0];
+        BubbleSort.sort(a);
+        Assert.assertTrue(a.length == 0);
+    }
+
+    @Test
+    public void handleMultipleElement() {
+        int[] a = new int[] {5, 2, 3, 6, 8};
+        BubbleSort.sort(a);
+        Assert.assertArrayEquals(a, new int[] {2, 3, 5, 6, 8});
+    }
+
+    @Test
+    public void arrayWithRepeatingElement() {
+        int[] a = new int[] {5, 2, 3, 3, 6, 6, 8};
+        BubbleSort.sort(a);
+        Assert.assertArrayEquals(a, new int[] {2, 3, 3, 5, 6, 6, 8});
+    }
 }
--- src/test/java/sorting/insertsort/InsertSortTest.java
+++ src/test/java/sorting/insertsort/InsertSortTest.java
@@ -1,8 +1,31 @@
 package sorting.insertsort;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author asif.hossain
  * @since 7/27/17.
  */
 public class InsertSortTest {
+    @Test
+    public void emptyArrayReturnsEmpty() {
+        int[] a = new int[0];
+        InsertSort.sort(a);
+        Assert.assertTrue(a.length == 0);
+    }
+
+    @Test
+    public void handleMultipleElement() {
+        int[] a = new int[] {5, 2, 3, 6, 8};
+        InsertSort.sort(a);
+        Assert.assertArrayEquals(a, new int[] {2, 3, 5, 6, 8});
+    }
+
+    @Test
+    public void arrayWithRepeatingElement() {
+        int[] a = new int[] {5, 2, 3, 3, 6, 6, 8};
+        InsertSort.sort(a);
+        Assert.assertArrayEquals(a, new int[] {2, 3, 3, 5, 6, 6, 8});
+    }
 }
